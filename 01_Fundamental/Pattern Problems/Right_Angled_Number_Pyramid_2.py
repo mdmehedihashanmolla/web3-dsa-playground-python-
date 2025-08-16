82e551497b8eb27abd46741a95a82c13b4bed10e
MD MEHEDI HASHAN MOLLA <mdmehedihashanmolla@gmail.com>
2025-08-16 20:35:30 +0530

    init

---
+++ 01_Fundamental/Pattern Problems/Right_Angled_Number_Pyramid_2.py
@@ -0,0 +1,7 @@
+def Right_Angled_Number_Pyramid_ii(n):
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            print(i, end=" ")
+        print()
+
+Right_Angled_Number_Pyramid_ii(5)
